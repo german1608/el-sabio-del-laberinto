4dae35e28018558e7953af3b43f48ca5af86787a
German Robayo Paz <germanrobayo33@gmail.com>
2018-10-26 17:37:16 -0400

    cambia la firma de opcionesPosiblesConMsj para que use (String,String)

--- Sabio.hs
+++ Sabio.hs
@@ -3,23 +3,24 @@ import Laberinto
 import Data.Char (digitToInt, isDigit)
 import Data.List (intercalate)
 
-opcionesPosiblesConMsj :: [(Char, String)]
-opcionesPosiblesConMsj = [('1', "Hablar de un laberinto nuevo"),
-    ('2', "Quiero darte una ruta"),
-    ('3', "Reportar pared abierta"),
-    ('4', "Reportar derrumbe"),
-    ('5', "Reportar tesoro tomado"),
-    ('6', "Reportar tesoro hallado"),
-    ('7', "Dar nombre al laberinto"),
-    ('8', "Hablar de un laberinto de nombre conocido")]
+opcionesPosiblesConMsj :: [(String, String)]
+opcionesPosiblesConMsj = [
+    ("1", "Hablar de un laberinto nuevo"),
+    ("2", "Quiero darte una ruta"),
+    ("3", "Reportar pared abierta"),
+    ("4", "Reportar derrumbe"),
+    ("5", "Reportar tesoro tomado"),
+    ("6", "Reportar tesoro hallado"),
+    ("7", "Dar nombre al laberinto"),
+    ("8", "Hablar de un laberinto de nombre conocido")]
 
 opciones :: [Int]
-opciones = map (digitToInt . fst) opcionesPosiblesConMsj
+opciones = map (read . fst) opcionesPosiblesConMsj
 
 -- | Funcion que imprime el menu con las posibles opciones
 imprimirMenu :: IO ()
 imprimirMenu = putStr $
-    foldl (\acc (x,y) -> acc ++ [x] ++ ") " ++ y ++ "\n") "" opcionesPosiblesConMsj
+    foldl (\acc (x,y) -> acc ++ x ++ ") " ++ y ++ "\n") "" opcionesPosiblesConMsj
 
 -- | Funcion que hace prompt al user por las opciones adecuadas
 prompt :: IO ()
@@ -28,7 +29,7 @@ prompt = do
     o:_ <- getLine
     if not ((isDigit o) && ((digitToInt o) `elem` opciones)) then do
         putStrLn "Opción incorrecta"
-        putStr "Las opciones correctas son:"
+        putStr "Las opciones correctas son: "
         putStrLn $ intercalate ", " $ map show opciones
     else
         putStr "op"
